7967e68b195dec730d10cdfb27f8d5e7b463db59
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-01-03 10:13:04 -0600

    Prompting 3 words form user

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -13,7 +13,17 @@ public class ConsoleExercises {
 
          System.out.println("The Integer You Entered: --> \"" + userInput + "\" <--");
 
+        // Prompting user to enter 3 words
+        System.out.print("Please Enter Three words: ");
+        String wordOne = scanner.next();
+        String wordTwo = scanner.next();
+        String wordThree = scanner.next();
 
+        // If I enter to little words, code will not run until I enter 3 words
+        // If I enter to many words I will receive an error
+        System.out.println("1st Word Entered: --> \"" + wordOne + "\" <--");
+        System.out.println("2nd Word Entered: --> \"" + wordTwo + "\" <--");
+        System.out.println("3rd Word Entered: --> \"" + wordThree + "\" <--");
 
     }
 }
