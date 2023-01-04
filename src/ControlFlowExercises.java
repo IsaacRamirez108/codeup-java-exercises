2c7a3fdd5431a56de4677d38de9423b3a7099d3d
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-01-04 13:55:02 -0600

    update

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,11 +7,17 @@ public class ControlFlowExercises {
 
         System.out.println("");
 
-        int a = 2;
+        // int a = 2;
+        // do {
+        //     System.out.println("num: " + a);
+        //     a += 2;
+        // } while (a <= 100);
+
+        // Refactoring code above to decrement by 5
+        int a = 100;
         do {
             System.out.println("num: " + a);
-            a += 2;
-        } while (a <= 100);
-
+            a -= 5;
+        } while (a >= -10);
     }
 }
