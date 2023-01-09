87d26e66bf537c8208e2739d403acc128292bcfa
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-01-09 13:54:59 -0600

    person exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -14,10 +14,22 @@ public class ControlFlowExercises {
         // } while (a <= 100);
 
         // Refactoring code above to decrement by 5
-        int a = 100;
-        do {
-            System.out.println("num: " + a);
-            a -= 5;
-        } while (a >= -10);
+        // int a = 100;
+        // do {
+            // System.out.println("num: " + a);
+            // a -= 5;
+        // } while (a >= -10);
+
+        int a = 2;
+         do {
+             System.out.println("num: " + a);
+             a *= 2;
+         } while (a <= 1000000);
+
+//        for(int a = 2; a <= 100; a++){
+//            System.out.println("num: " + a);
+//            a += 2;
+//        }
+
     }
 }
---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String mssg = "\nWe don't need no education\nWe don't need no thought control";
+        System.out.println(mssg);
+
+        String newMssg = "Check \"this\" out!, \"s inside of \"s! \n";
+        System.out.println(newMssg);
+
+//        String mssgThree = "In windows, the main drive is usually C:\";
+//        System.out.println(mssgThree);
+
+//        String mssgFour = "I can do backslashes \, double backslashes, \\ \nand the amazing triple backslash \\\!";
+//        System.out.println(newMssg);
+    }
+}
