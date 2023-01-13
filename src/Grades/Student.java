42573fc699cc9e08f3b3e20bda9ef9a9dc7446b2
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-01-13 10:43:34 -0600

    creating GradesApplication class

--- src/Grades/Student.java
+++ src/Grades/Student.java
@@ -5,4 +5,31 @@ import java.util.ArrayList;
 public class Student {
     private String name;
     private ArrayList<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // Returns Students name
+    public String getName(){
+        return name;
+    }
+
+    // Adds the given grade to the grade property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // Returns the average of the students grades
+    public double getGradeAverage(){
+        int sum = 0;
+        for(int i =0; i < grades.size(); i++){
+            sum += grades.get(i);
+        }
+        return sum / grades.size();
+    }
+
+
+
 }
